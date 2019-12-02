bdf7e7419d8ff4dd8073c734c5031e5bf05d78d8
Magdalena Gola <turcza.magdalena@gmail.com>
2019-12-02 14:16:36 +0100

    Add createUser method

---
+++ src/main/java/UserFactory.java
@@ -0,0 +1,16 @@
+public class UserFactory {
+    public User createUser(String userName) {
+        switch (userName) {
+            case "Manager":
+                return new Manager();
+            case "Mentor":
+                return new Mentor();
+            case "Employee":
+                return new Employee();
+            case "Student":
+                return new Student();
+            default:
+                return null;
+        }
+    }
+}
