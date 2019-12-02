f24da145f63772a225ede1e6cfdbf3d7133f8cc7
Magdalena Gola <turcza.magdalena@gmail.com>
2019-12-02 14:16:09 +0100

    Add run, tryLogin and getUserLoginInfo methods

---
+++ src/main/java/LoginController.java
@@ -0,0 +1,29 @@
+public class LoginController {
+
+    private String login;
+    private String password;
+
+
+    public User run(){
+        getUserLoginInfo();
+        return tryLogin();
+    }
+
+    private User tryLogin() {
+        SQLController sqlController = new SQLController();
+        UserFactory userFactory = new UserFactory();
+        if (sqlController.isUserDataCorrect(login, password)){
+                return userFactory.createUser(sqlController.getUserType(login));
+        };
+        return run();
+    }
+
+    private void getUserLoginInfo() {
+        View.printMessage("Login:");
+        login = View.getInput();
+        View.printMessage("Password:");
+        password = View.getInput();
+    }
+
+
+}
