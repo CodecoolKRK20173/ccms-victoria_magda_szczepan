9f8f59a8b12a83f3fc18f03f2589f809fe6a2cff
szczepanTopolski <szczepanPL678@gmail.com>
2019-12-03 09:23:50 +0100

    Add packages, add abstract user

--- src/main/java/controllers/RootController.java
+++ src/main/java/controllers/RootController.java
@@ -1,6 +1,10 @@
+package controllers;
+
+import controllers.LoginController;
+
 public class RootController {
     public void run() {
-        LoginController loginController = new LoginControler();
+        LoginController loginController = new LoginController();
         User user = loginController.run();
         user.runController();
     }
