d0d78d3170376c663f79f5264338ef9e1d054cf0
Magdalena Gola <turcza.magdalena@gmail.com>
2019-12-03 11:25:44 +0100

    Add employee class and controller

---
+++ src/main/java/controllers/EmployeeController.java
@@ -0,0 +1,29 @@
+package controllers;
+
+import users.Employee;
+import users.Mentor;
+import view.View;
+
+public class EmployeeController {
+
+    public void run(Employee employee){
+        System.out.println("YOU ARE AN EMPLOYEE");
+        String[] options = {"View a list of students.", "Logout."};
+            View.displayMenu(options);
+        int userChoice = View.getUserChoice(options.length);
+            switch (userChoice) {
+                case 1:
+                    viewStudents();
+                    break;
+                case 2:
+                    LoginController controller = new LoginController();
+                    controller.logOut();
+                    break;
+            }
+    }
+
+    private void viewStudents(){
+            }
+
+
+    }
